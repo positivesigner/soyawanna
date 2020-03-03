55b9b1bffa7185b6d5520a3ca59e1ee163ddcf6a
Tom Stone <stonethomasjames@email.com>
2020-03-02 22:59:32 -0800

    root script

---
+++ git checkout.cmd
@@ -0,0 +1,7 @@
+@echo off
+"C:\TJBF\zPortableInstalls\GitHubDesktopPortable\App\GitHubDesktop\app-1.6.1\resources\app\git\cmd\git.exe" 
+rem git checkout -b test-branch 56a4e5c08
+rem change to another branch and bring the changes to the new branch for 'git checkout.cmd'
+rem git branch -d test-branch
+rem git push origin -d test-branch   ( Git v1.7.0 )
+rem or  git push origin :test-branch   ( Git older )
