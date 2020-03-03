cb484d6d7b75af75b88e9dbcdcf366f398c82876
Tom Stone <stonethomasjames@email.com>
2020-03-02 23:08:41 -0800

    root script

--- git checkout.cmd
+++ git checkout.cmd
@@ -1,7 +1,7 @@
 @echo off
-"C:\TJBF\zPortableInstalls\GitHubDesktopPortable\App\GitHubDesktop\app-1.6.1\resources\app\git\cmd\git.exe" 
-rem git checkout -b test-branch 56a4e5c08
-rem change to another branch and bring the changes to the new branch for 'git checkout.cmd'
-rem git branch -d test-branch
-rem git push origin -d test-branch   ( Git v1.7.0 )
-rem or  git push origin :test-branch   ( Git older )
+rem You have to copy this up to the parent directory, because changing this file will stop creating the new branch
+cd UrDir
+"..\PortableGit\cmd\git.exe" checkout -b test-branch UrHash
+rem "..\PortableGit\cmd\git.exe" branch -d test-branch
+rem delete a branch: git push origin -d test-branch   ( Git v1.7.0 )
+rem or:  git push origin :test-branch   ( Git older )
