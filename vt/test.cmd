9e6671953adc391a0bbef71d374230ccc95908c7
Tom <stonethomasjames@email.com>
2021-01-19 22:18:45 -0800

    tv

--- vt/test.cmd
+++
@@ -1,8 +0,0 @@
-rd  /s /q "C:\Temp\Chrome dev session"
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --incognito --user-data-dir="C://Temp//Chrome dev session" --disable-web-security
-rd  /s /q "C:\Temp\Chrome dev session"
-rem https://stackoverflow.com/questions/15268604/html5-track-captions-not-showing
-rem Disable same origin policy in Chrome
-rem https://stackoverflow.com/questions/3102819/disable-same-origin-policy-in-chrome
-rem SubRip to WebVTT converter
-rem https://atelier.u-sub.net/srt2vtt/
